19ebb4915493e78195ce4fcce161b26bb3795705
sandipan-Coder <palsandipan68@gmail.com>
2025-02-06 12:07:08 +0530

    change in index file of Backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,7 +10,7 @@ dotenv.config({
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen("https://ambhika-jwellers.onrender.com/", () => {
         console.log(`Server is running at port: ${process.env.PORT}`);
     })
 })
