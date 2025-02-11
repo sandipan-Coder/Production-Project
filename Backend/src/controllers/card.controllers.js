76c19d6347b766dea98cf05a87d8232304d36063
sandipan-Coder <palsandipan68@gmail.com>
2025-02-12 00:43:58 +0530

    some changes in multer and cloudinary file

--- Backend/src/controllers/card.controllers.js
+++ Backend/src/controllers/card.controllers.js
@@ -12,7 +12,6 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET
-
 });
 
 // Authenticated user can create a new card and delete a card
--- Backend/src/utils/cloudinary.js
+++ Backend/src/utils/cloudinary.js
@@ -2,9 +2,6 @@ import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"    // This is use for file system
 import dotenv from "dotenv";
 
-// dotenv.config({
-//     path: '..../.env'
-// })
 dotenv.config();
 
 
@@ -13,7 +10,6 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET
-
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -24,10 +20,8 @@ const uploadOnCloudinary = async (localFilePath) => {
         // Upload the file on cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "image",
-            // public_id: "image",
         })
         // File has been  uploaded successfully
-        // console.log("File is uploaded on cloudinary: ", response.url);
         fs.unlinkSync(localFilePath)
         return response;
 
