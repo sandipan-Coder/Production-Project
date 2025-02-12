f1e91ea6d922274fe294a4b2892dd63920c2fc9c
sandipan-Coder <palsandipan68@gmail.com>
2025-02-13 01:46:54 +0530

    change the limit of the request file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,8 +14,8 @@ app.use(
 
 const _dirname = path.resolve();
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 
 // Routes Declaration
